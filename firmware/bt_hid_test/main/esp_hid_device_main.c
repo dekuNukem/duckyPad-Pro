89e05b5670d02ba636753afa96ee90e16a28ea84
dekuNukem <dekunukem@gmail.com>
2024-07-23 23:57:01 +0100

    ported over keyboard but not working

--- firmware/bt_hid_test/main/esp_hid_device_main.c
+++ firmware/bt_hid_test/main/esp_hid_device_main.c
@@ -43,70 +43,47 @@ typedef struct
 
 static local_param_t s_ble_hid_param = {0};
 
-const unsigned char mediaReportMap[] = {
-    0x05, 0x0C,        // Usage Page (Consumer)
-    0x09, 0x01,        // Usage (Consumer Control)
+const unsigned char hid_descriptor_map[] = { //7 bytes input (modifiers, resrvd, keys*5), 1 byte output
+    0x05, 0x01,        // Usage Page (Generic Desktop Ctrls)
+    0x09, 0x06,        // Usage (Keyboard)
     0xA1, 0x01,        // Collection (Application)
-    0x85, 0x03,        //   Report ID (3)
-    0x09, 0x02,        //   Usage (Numeric Key Pad)
-    0xA1, 0x02,        //   Collection (Logical)
-    0x05, 0x09,        //     Usage Page (Button)
-    0x19, 0x01,        //     Usage Minimum (0x01)
-    0x29, 0x0A,        //     Usage Maximum (0x0A)
-    0x15, 0x01,        //     Logical Minimum (1)
-    0x25, 0x0A,        //     Logical Maximum (10)
-    0x75, 0x04,        //     Report Size (4)
-    0x95, 0x01,        //     Report Count (1)
-    0x81, 0x00,        //     Input (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0xC0,              //   End Collection
-    0x05, 0x0C,        //   Usage Page (Consumer)
-    0x09, 0x86,        //   Usage (Channel)
-    0x15, 0xFF,        //   Logical Minimum (-1)
-    0x25, 0x01,        //   Logical Maximum (1)
-    0x75, 0x02,        //   Report Size (2)
-    0x95, 0x01,        //   Report Count (1)
-    0x81, 0x46,        //   Input (Data,Var,Rel,No Wrap,Linear,Preferred State,Null State)
-    0x09, 0xE9,        //   Usage (Volume Increment)
-    0x09, 0xEA,        //   Usage (Volume Decrement)
+    0x85, 0x01,        //   Report ID (1)
+    0x05, 0x07,        //   Usage Page (Kbrd/Keypad)
+    0x19, 0xE0,        //   Usage Minimum (0xE0)
+    0x29, 0xE7,        //   Usage Maximum (0xE7)
     0x15, 0x00,        //   Logical Minimum (0)
+    0x25, 0x01,        //   Logical Maximum (1)
     0x75, 0x01,        //   Report Size (1)
-    0x95, 0x02,        //   Report Count (2)
+    0x95, 0x08,        //   Report Count (8)
     0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x09, 0xE2,        //   Usage (Mute)
-    0x09, 0x30,        //   Usage (Power)
-    0x09, 0x83,        //   Usage (Recall Last)
-    0x09, 0x81,        //   Usage (Assign Selection)
-    0x09, 0xB0,        //   Usage (Play)
-    0x09, 0xB1,        //   Usage (Pause)
-    0x09, 0xB2,        //   Usage (Record)
-    0x09, 0xB3,        //   Usage (Fast Forward)
-    0x09, 0xB4,        //   Usage (Rewind)
-    0x09, 0xB5,        //   Usage (Scan Next Track)
-    0x09, 0xB6,        //   Usage (Scan Previous Track)
-    0x09, 0xB7,        //   Usage (Stop)
-    0x15, 0x01,        //   Logical Minimum (1)
-    0x25, 0x0C,        //   Logical Maximum (12)
-    0x75, 0x04,        //   Report Size (4)
     0x95, 0x01,        //   Report Count (1)
-    0x81, 0x00,        //   Input (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x09, 0x80,        //   Usage (Selection)
-    0xA1, 0x02,        //   Collection (Logical)
-    0x05, 0x09,        //     Usage Page (Button)
-    0x19, 0x01,        //     Usage Minimum (0x01)
-    0x29, 0x03,        //     Usage Maximum (0x03)
-    0x15, 0x01,        //     Logical Minimum (1)
-    0x25, 0x03,        //     Logical Maximum (3)
-    0x75, 0x02,        //     Report Size (2)
-    0x81, 0x00,        //     Input (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0xC0,              //   End Collection
+    0x75, 0x08,        //   Report Size (8)
     0x81, 0x03,        //   Input (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0x95, 0x05,        //   Report Count (5)
+    0x75, 0x01,        //   Report Size (1)
+    0x05, 0x08,        //   Usage Page (LEDs)
+    0x19, 0x01,        //   Usage Minimum (Num Lock)
+    0x29, 0x05,        //   Usage Maximum (Kana)
+    0x91, 0x02,        //   Output (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+    0x95, 0x01,        //   Report Count (1)
+    0x75, 0x03,        //   Report Size (3)
+    0x91, 0x03,        //   Output (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+    0x95, 0x05,        //   Report Count (5)
+    0x75, 0x08,        //   Report Size (8)
+    0x15, 0x00,        //   Logical Minimum (0)
+    0x25, 0x65,        //   Logical Maximum (101)
+    0x05, 0x07,        //   Usage Page (Kbrd/Keypad)
+    0x19, 0x00,        //   Usage Minimum (0x00)
+    0x29, 0x65,        //   Usage Maximum (0x65)
+    0x81, 0x00,        //   Input (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
     0xC0,              // End Collection
+    // 65 bytes
 };
 
 static esp_hid_raw_report_map_t ble_report_maps[] = {
     {
-        .data = mediaReportMap,
-        .len = sizeof(mediaReportMap)
+        .data = hid_descriptor_map,
+        .len = sizeof(hid_descriptor_map)
     }
 };
 
@@ -131,161 +108,16 @@ void ble_hid_task_shut_down(void)
     ;
 }
 
-#define HID_CC_RPT_MUTE                 1
-#define HID_CC_RPT_POWER                2
-#define HID_CC_RPT_LAST                 3
-#define HID_CC_RPT_ASSIGN_SEL           4
-#define HID_CC_RPT_PLAY                 5
-#define HID_CC_RPT_PAUSE                6
-#define HID_CC_RPT_RECORD               7
-#define HID_CC_RPT_FAST_FWD             8
-#define HID_CC_RPT_REWIND               9
-#define HID_CC_RPT_SCAN_NEXT_TRK        10
-#define HID_CC_RPT_SCAN_PREV_TRK        11
-#define HID_CC_RPT_STOP                 12
+#define HID_RPT_ID_CC_IN        1   // Consumer Control input report ID
+#define HID_CC_IN_RPT_LEN       8   // Consumer Control input report Len
 
-#define HID_CC_RPT_CHANNEL_UP           0x10
-#define HID_CC_RPT_CHANNEL_DOWN         0x30
-#define HID_CC_RPT_VOLUME_UP            0x40
-#define HID_CC_RPT_VOLUME_DOWN          0x80
-
-// HID Consumer Control report bitmasks
-#define HID_CC_RPT_NUMERIC_BITS         0xF0
-#define HID_CC_RPT_CHANNEL_BITS         0xCF
-#define HID_CC_RPT_VOLUME_BITS          0x3F
-#define HID_CC_RPT_BUTTON_BITS          0xF0
-#define HID_CC_RPT_SELECTION_BITS       0xCF
-
-// Macros for the HID Consumer Control 2-byte report
-#define HID_CC_RPT_SET_NUMERIC(s, x)    (s)[0] &= HID_CC_RPT_NUMERIC_BITS;   (s)[0] = (x)
-#define HID_CC_RPT_SET_CHANNEL(s, x)    (s)[0] &= HID_CC_RPT_CHANNEL_BITS;   (s)[0] |= ((x) & 0x03) << 4
-#define HID_CC_RPT_SET_VOLUME_UP(s)     (s)[0] &= HID_CC_RPT_VOLUME_BITS;    (s)[0] |= 0x40
-#define HID_CC_RPT_SET_VOLUME_DOWN(s)   (s)[0] &= HID_CC_RPT_VOLUME_BITS;    (s)[0] |= 0x80
-#define HID_CC_RPT_SET_BUTTON(s, x)     (s)[1] &= HID_CC_RPT_BUTTON_BITS;    (s)[1] |= (x)
-#define HID_CC_RPT_SET_SELECTION(s, x)  (s)[1] &= HID_CC_RPT_SELECTION_BITS; (s)[1] |= ((x) & 0x03) << 4
-
-// HID Consumer Usage IDs (subset of the codes available in the USB HID Usage Tables spec)
-#define HID_CONSUMER_POWER          48  // Power
-#define HID_CONSUMER_RESET          49  // Reset
-#define HID_CONSUMER_SLEEP          50  // Sleep
-
-#define HID_CONSUMER_MENU           64  // Menu
-#define HID_CONSUMER_SELECTION      128 // Selection
-#define HID_CONSUMER_ASSIGN_SEL     129 // Assign Selection
-#define HID_CONSUMER_MODE_STEP      130 // Mode Step
-#define HID_CONSUMER_RECALL_LAST    131 // Recall Last
-#define HID_CONSUMER_QUIT           148 // Quit
-#define HID_CONSUMER_HELP           149 // Help
-#define HID_CONSUMER_CHANNEL_UP     156 // Channel Increment
-#define HID_CONSUMER_CHANNEL_DOWN   157 // Channel Decrement
-
-#define HID_CONSUMER_PLAY           176 // Play
-#define HID_CONSUMER_PAUSE          177 // Pause
-#define HID_CONSUMER_RECORD         178 // Record
-#define HID_CONSUMER_FAST_FORWARD   179 // Fast Forward
-#define HID_CONSUMER_REWIND         180 // Rewind
-#define HID_CONSUMER_SCAN_NEXT_TRK  181 // Scan Next Track
-#define HID_CONSUMER_SCAN_PREV_TRK  182 // Scan Previous Track
-#define HID_CONSUMER_STOP           183 // Stop
-#define HID_CONSUMER_EJECT          184 // Eject
-#define HID_CONSUMER_RANDOM_PLAY    185 // Random Play
-#define HID_CONSUMER_SELECT_DISC    186 // Select Disk
-#define HID_CONSUMER_ENTER_DISC     187 // Enter Disc
-#define HID_CONSUMER_REPEAT         188 // Repeat
-#define HID_CONSUMER_STOP_EJECT     204 // Stop/Eject
-#define HID_CONSUMER_PLAY_PAUSE     205 // Play/Pause
-#define HID_CONSUMER_PLAY_SKIP      206 // Play/Skip
-
-#define HID_CONSUMER_VOLUME         224 // Volume
-#define HID_CONSUMER_BALANCE        225 // Balance
-#define HID_CONSUMER_MUTE           226 // Mute
-#define HID_CONSUMER_BASS           227 // Bass
-#define HID_CONSUMER_VOLUME_UP      233 // Volume Increment
-#define HID_CONSUMER_VOLUME_DOWN    234 // Volume Decrement
-
-#define HID_RPT_ID_CC_IN        3   // Consumer Control input report ID
-#define HID_CC_IN_RPT_LEN       2   // Consumer Control input report Len
-
-#define HID_TX_BUF_SIZE HID_CC_IN_RPT_LEN
-
-void esp_hidd_send_consumer_value(uint8_t key_cmd, bool key_pressed)
+void ble_kb_send(uint8_t* hid_buf, uint8_t bufsize)
 {
-    uint8_t buffer[HID_TX_BUF_SIZE] = {0, 0};
-    if (key_pressed) {
-        switch (key_cmd) {
-        case HID_CONSUMER_CHANNEL_UP:
-            HID_CC_RPT_SET_CHANNEL(buffer, HID_CC_RPT_CHANNEL_UP);
-            break;
-
-        case HID_CONSUMER_CHANNEL_DOWN:
-            HID_CC_RPT_SET_CHANNEL(buffer, HID_CC_RPT_CHANNEL_DOWN);
-            break;
-
-        case HID_CONSUMER_VOLUME_UP:
-            HID_CC_RPT_SET_VOLUME_UP(buffer);
-            break;
-
-        case HID_CONSUMER_VOLUME_DOWN:
-            HID_CC_RPT_SET_VOLUME_DOWN(buffer);
-            break;
-
-        case HID_CONSUMER_MUTE:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_MUTE);
-            break;
-
-        case HID_CONSUMER_POWER:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_POWER);
-            break;
-
-        case HID_CONSUMER_RECALL_LAST:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_LAST);
-            break;
-
-        case HID_CONSUMER_ASSIGN_SEL:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_ASSIGN_SEL);
-            break;
-
-        case HID_CONSUMER_PLAY:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_PLAY);
-            break;
-
-        case HID_CONSUMER_PAUSE:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_PAUSE);
-            break;
-
-        case HID_CONSUMER_RECORD:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_RECORD);
-            break;
-
-        case HID_CONSUMER_FAST_FORWARD:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_FAST_FWD);
-            break;
-
-        case HID_CONSUMER_REWIND:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_REWIND);
-            break;
-
-        case HID_CONSUMER_SCAN_NEXT_TRK:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_SCAN_NEXT_TRK);
-            break;
-
-        case HID_CONSUMER_SCAN_PREV_TRK:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_SCAN_PREV_TRK);
-            break;
-
-        case HID_CONSUMER_STOP:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_STOP);
-            break;
-
-        default:
-            break;
-        }
-    }
-    for (size_t i = 0; i < HID_TX_BUF_SIZE; i++)
-        printf("%02d ", buffer[i]);
+    for (size_t i = 0; i < bufsize; i++)
+        printf("%02d ", hid_buf[i]);
     printf("\n");
-    esp_hidd_dev_input_set(s_ble_hid_param.hid_dev, 0, HID_RPT_ID_CC_IN, buffer, HID_CC_IN_RPT_LEN);
-    return;
+    int result = esp_hidd_dev_input_set(s_ble_hid_param.hid_dev, 0, HID_RPT_ID_CC_IN, hid_buf, HID_CC_IN_RPT_LEN);
+    printf("send result: %d\n", result);
 }
 
 static void ble_hidd_event_callback(void *handler_args, esp_event_base_t base, int32_t id, void *event_data)
@@ -345,11 +177,17 @@ static void ble_hidd_event_callback(void *handler_args, esp_event_base_t base, i
     return;
 }
 
+#define HID_TX_BUF_SIZE HID_CC_IN_RPT_LEN
+uint8_t test_buf[HID_TX_BUF_SIZE];
 void my_test(void)
 {
-    esp_hidd_send_consumer_value(HID_CONSUMER_VOLUME_UP, true);
+    memset(test_buf, 0, HID_TX_BUF_SIZE);
+    test_buf[2] = 0x2C;
+    ble_kb_send(test_buf, HID_TX_BUF_SIZE);
     vTaskDelay(100 / portTICK_PERIOD_MS);
-    esp_hidd_send_consumer_value(HID_CONSUMER_VOLUME_UP, false);
+
+    memset(test_buf, 0, HID_TX_BUF_SIZE);
+    ble_kb_send(test_buf, HID_TX_BUF_SIZE);
     vTaskDelay(100 / portTICK_PERIOD_MS);
 }
 
@@ -358,40 +196,26 @@ void app_main(void)
     switch_init();
     xTaskCreate(kb_scan_task, "kb_scan_task", SW_SCAN_TASK_STACK_SIZE, NULL, 2, NULL);
     esp_err_t ret;
-#if HID_DEV_MODE == HIDD_IDLE_MODE
-    ESP_LOGE(TAG, "Please turn on BT HID device or BLE!");
-    return;
-#endif
+
     ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         ESP_ERROR_CHECK(nvs_flash_erase());
         ret = nvs_flash_init();
     }
-    ESP_ERROR_CHECK( ret );
+    ESP_ERROR_CHECK(ret);
 
     ESP_LOGI(TAG, "setting hid gap, mode:%d", HID_DEV_MODE);
     ret = esp_hid_gap_init(HID_DEV_MODE);
-    ESP_ERROR_CHECK( ret );
+    ESP_ERROR_CHECK(ret);
 
-#if CONFIG_BT_BLE_ENABLED || CONFIG_BT_NIMBLE_ENABLED
-#if CONFIG_EXAMPLE_HID_DEVICE_ROLE == 2
     ret = esp_hid_ble_gap_adv_init(ESP_HID_APPEARANCE_KEYBOARD, ble_hid_config.device_name);
-#elif CONFIG_EXAMPLE_HID_DEVICE_ROLE == 3
-    ret = esp_hid_ble_gap_adv_init(ESP_HID_APPEARANCE_MOUSE, ble_hid_config.device_name);
-#else
-    ret = esp_hid_ble_gap_adv_init(ESP_HID_APPEARANCE_GENERIC, ble_hid_config.device_name);
-#endif
-    ESP_ERROR_CHECK( ret );
-#if CONFIG_BT_BLE_ENABLED
+    ESP_ERROR_CHECK(ret);
     if ((ret = esp_ble_gatts_register_callback(esp_hidd_gatts_event_handler)) != ESP_OK) {
         ESP_LOGE(TAG, "GATTS register callback failed: %d", ret);
         return;
     }
-#endif
     ESP_LOGI(TAG, "setting ble device");
-    ESP_ERROR_CHECK(
-        esp_hidd_dev_init(&ble_hid_config, ESP_HID_TRANSPORT_BLE, ble_hidd_event_callback, &s_ble_hid_param.hid_dev));
-#endif
+    ESP_ERROR_CHECK(esp_hidd_dev_init(&ble_hid_config, ESP_HID_TRANSPORT_BLE, ble_hidd_event_callback, &s_ble_hid_param.hid_dev));
 
     while(1)
     {
