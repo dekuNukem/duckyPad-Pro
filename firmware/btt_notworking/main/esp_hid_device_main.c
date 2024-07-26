bb4e3e3b540cb6bafa7381ef8b9c787b07c7f2cd
dekuNukem <dekunukem@gmail.com>
2024-07-26 13:17:33 +0100

    put in my own HID keyboard desp table, still working

--- firmware/btt_notworking/main/esp_hid_device_main.c
+++ firmware/btt_notworking/main/esp_hid_device_main.c
@@ -43,41 +43,29 @@ typedef struct
 
 static local_param_t s_ble_hid_param = {0};
 
-const unsigned char hid_descriptor_map[] = { //7 bytes input (modifiers, resrvd, keys*5), 1 byte output
+const unsigned char hid_descriptor_map[] = {
+    // Report ID 1: Keyboard
     0x05, 0x01,        // Usage Page (Generic Desktop Ctrls)
     0x09, 0x06,        // Usage (Keyboard)
     0xA1, 0x01,        // Collection (Application)
     0x85, 0x01,        //   Report ID (1)
     0x05, 0x07,        //   Usage Page (Kbrd/Keypad)
+    0x75, 0x01,        //   Report Size (1)
+    0x95, 0x08,        //   Report Count (8)
     0x19, 0xE0,        //   Usage Minimum (0xE0)
     0x29, 0xE7,        //   Usage Maximum (0xE7)
     0x15, 0x00,        //   Logical Minimum (0)
     0x25, 0x01,        //   Logical Maximum (1)
-    0x75, 0x01,        //   Report Size (1)
-    0x95, 0x08,        //   Report Count (8)
     0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x95, 0x01,        //   Report Count (1)
-    0x75, 0x08,        //   Report Size (8)
-    0x81, 0x03,        //   Input (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x95, 0x05,        //   Report Count (5)
-    0x75, 0x01,        //   Report Size (1)
-    0x05, 0x08,        //   Usage Page (LEDs)
-    0x19, 0x01,        //   Usage Minimum (Num Lock)
-    0x29, 0x05,        //   Usage Maximum (Kana)
-    0x91, 0x02,        //   Output (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
-    0x95, 0x01,        //   Report Count (1)
-    0x75, 0x03,        //   Report Size (3)
-    0x91, 0x03,        //   Output (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
-    0x95, 0x05,        //   Report Count (5)
+    0x95, 0x06,        //   Report Count (3)
     0x75, 0x08,        //   Report Size (8)
     0x15, 0x00,        //   Logical Minimum (0)
-    0x25, 0x65,        //   Logical Maximum (101)
+    0x25, 0x94,        //   Logical Maximum (0x94) originally 65, 73 supports F13 - F24, 94 for japanese and korean language keys
     0x05, 0x07,        //   Usage Page (Kbrd/Keypad)
     0x19, 0x00,        //   Usage Minimum (0x00)
-    0x29, 0x65,        //   Usage Maximum (0x65)
+    0x29, 0x94,        //   Usage Maximum (0x94) originally 65, 73 supports F13 - F24
     0x81, 0x00,        //   Input (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
     0xC0,              // End Collection
-    // 65 bytes
 };
 
 static esp_hid_raw_report_map_t ble_report_maps[] = {
