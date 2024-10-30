0418ffc8ec12119f5cdc58b94acfd48a6727e5eb
dekuNukem <dekunukem@gmail.com>
2024-10-30 14:18:34 +0000

    updated syntax highlighter

--- firmware/dpp_fw/main/ds_vm.c
+++ firmware/dpp_fw/main/ds_vm.c
@@ -182,7 +182,7 @@ uint8_t readkey_blocking(void)
   clear_sw_re_queue();
   while(1)
   {
-    delay_ms(50);
+    delay_ms(10);
     if(xQueueReceive(rotary_encoder_event_queue, &re_event, 0))
       return re_event_to_swid(&re_event)+1;
     if(xQueueReceive(switch_event_queue, &sw_event, 0) && sw_event.type == SW_EVENT_SHORT_PRESS)
