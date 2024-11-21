4720ee23909e43d31db3fae118fb41d74db8455b
dekuNukem <dekunukem@gmail.com>
2024-11-21 11:18:17 +0000

    updated updating

--- pc_software/check_update.py
+++ pc_software/check_update.py
@@ -2,14 +2,14 @@ import json
 import socket
 import urllib.request
 
-pc_app_release_url = "https://api.github.com/repos/dekuNukem/duckyPad/releases/latest"
-firmware_url = 'https://api.github.com/repos/dekuNukem/duckyPad/contents/firmware?ref=master'
+pc_app_release_url = "https://api.github.com/repos/dekuNukem/duckyPad-Pro/releases/latest"
+firmware_url = 'https://api.github.com/repos/dekuNukem/duckyPad-Pro/contents/firmware?ref=master'
 
 def is_internet_available():
     try:
         socket.create_connection(("www.google.com", 80), timeout=1)
         return True
-    except OSError:
+    except Exception:
         pass
     return False
 
@@ -41,15 +41,15 @@ def get_firmware_update_status(current_version):
 	try:
 		file_list = json.loads(urllib.request.urlopen(firmware_url).read())
 		dfu_list = [x['name'] for x in file_list if 'name' in x and 'type' in x and x['type'] == 'file']
-		dfu_list = [d.replace('duckypad_v', '').replace('.dfu', '') for d in dfu_list if d.startswith('duckypad_v') and d.endswith('.dfu')]
+		dfu_list = [d.replace('DPP_FW_', '').replace('.bin', '').split('_')[0] for d in dfu_list if d.startswith('DPP_FW_') and d.endswith('.bin')]
 		dfu_list.sort(key=lambda s: list(map(int, s.split('.'))))
 		this_version = versiontuple(current_version)
 		remote_version = versiontuple(dfu_list[-1])
-		# print('this:', this_version, '\nremote:', remote_version)
+		print('this:', this_version, '\nremote:', remote_version)
 		return int(remote_version > this_version)
 	except Exception as e:
 		print('get_firmware_update_status:', e)
 		return 2
 
-# get_firmware_update_status('0.2.0')
-# print(has_update('0.0.9'))
+# result = get_firmware_update_status('0.2.0')
+# print(result)
