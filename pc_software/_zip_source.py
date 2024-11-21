642c1fac6c46c5519109832d24cf91ff3a6d99a1
dekuNukem <dekunukem@gmail.com>
2024-11-21 11:50:02 +0000

    added source zip

--- pc_software/_zip_source.py
+++ pc_software/_zip_source.py
@@ -1,6 +1,15 @@
 import os
 import sys
+import platform
 
 zip_file_name = "duckypad_config_latest_source.zip"
 os.system("rm -fv ./*.zip")
-os.system(f"7z.exe a {zip_file_name} ./ -x!_* -x!setup.py")
+
+current_os = platform.system()
+
+if current_os == "Windows":
+    os.system(f"7z.exe a {zip_file_name} ./ -x!_* -x!setup.py")
+elif current_os == "Darwin" or current_os == "Linux":
+    os.system(f"zip {zip_file_name} ./ -x '_*' -x 'setup.py'")
+else:
+    print("Unsupported OS")
