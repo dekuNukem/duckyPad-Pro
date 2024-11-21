ecd14c5ef1645342e9178f22d082d3726a07df2d
dekuNukem <dekunukem@gmail.com>
2024-11-21 11:51:46 +0000

    added source zip

--- pc_software/_zip_source.py
+++ pc_software/_zip_source.py
@@ -10,6 +10,6 @@ current_os = platform.system()
 if current_os == "Windows":
     os.system(f"7z.exe a {zip_file_name} ./ -x!_* -x!setup.py")
 elif current_os == "Darwin" or current_os == "Linux":
-    os.system(f"zip {zip_file_name} ./ -x '_*' -x 'setup.py'")
+    os.system(f"zip -r {zip_file_name} ./ -x '_*' -x 'setup.py'")
 else:
     print("Unsupported OS")
