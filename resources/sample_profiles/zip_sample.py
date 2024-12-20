a56af4d074ad55382dfe5eb7cab075e4552482ed
dekuNukem <dekunukem@gmail.com>
2024-12-20 17:10:20 +0000

    updated sample profile zip script

--- resources/sample_profiles/zip_sample.py
+++ resources/sample_profiles/zip_sample.py
@@ -1,13 +1,20 @@
 import os
+import shutil
 import sys
-import platform
 
-os.system("rm -fv ./*.zip")
-current_os = platform.system()
+for file in os.listdir('.'):
+    if file.endswith('.zip'):
+        os.remove(file)
+        print(f"Removed {file}")
 
-if current_os == "Windows":
-    os.system("7z a -r sample_profiles.zip ./sample_profiles/*")
-elif current_os == "Darwin" or current_os == "Linux":
-    os.system("zip -rv sample_profiles.zip ./sample_profiles/*")
+current_os = sys.platform
+
+if current_os.startswith('win'):
+    shutil.make_archive('sample_profiles', 'zip', './sample_profiles')
+    shutil.make_archive('keymaps', 'zip', './sample_profiles/keymaps')
+elif current_os.startswith('darwin') or current_os.startswith('linux'):
+    shutil.make_archive('sample_profiles', 'zip', './sample_profiles')
+    shutil.make_archive('keymaps', 'zip', './sample_profiles/keymaps')
 else:
     print("Unsupported OS")
+
