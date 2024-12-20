3db65c36dd6b5aec88cb370c3c3d9a0b91d639d6
dekuNukem <dekunukem@gmail.com>
2024-12-20 17:01:12 +0000

    no longer zip source at every push

--- push.sh
+++ push.sh
@@ -13,9 +13,9 @@ find . -type f -name "*.eggs*" -exec rm -f {} \;
 find . -name "__pycache__" -exec rm -rf {} \;
 find . -type f -name ".component_hash" -exec rm {} +
 
-cd ./pc_software
-python _zip_source.py
-python3 _zip_source.py
+# cd ./pc_software
+# python _zip_source.py
+# python3 _zip_source.py
 
 cd ../resources/sample_profiles
 python zip_sample.py
