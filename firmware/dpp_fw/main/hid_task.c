1333c97b2222cf32813cfe1cd47e1997caf201c6
dekuNukem <dekunukem@gmail.com>
2025-12-31 23:22:49 +0000

    added reserved bytes padding and removed motion wakeup in hid descriptor

--- firmware/dpp_fw/main/hid_task.c
+++ firmware/dpp_fw/main/hid_task.c
@@ -43,28 +43,36 @@ volatile uint8_t is_usb_hid_connected;
 #define CUSTOM_HID_EPIN_SIZE 63
 #define USBD_CUSTOMHID_OUTREPORT_BUF_SIZE CUSTOM_HID_EPIN_SIZE
 
-const uint8_t hid_report_descriptor[] = {
+const uint8_t hid_report_descriptor[] = 
+{
   // Report ID 1: Keyboard
   0x05, 0x01,        // Usage Page (Generic Desktop Ctrls)
   0x09, 0x06,        // Usage (Keyboard)
   0xA1, 0x01,        // Collection (Application)
-  0x85, 0x01,        //   Report ID (1)
-  0x05, 0x07,        //   Usage Page (Kbrd/Keypad)
-  0x75, 0x01,        //   Report Size (1)
-  0x95, 0x08,        //   Report Count (8)
-  0x19, 0xE0,        //   Usage Minimum (0xE0)
-  0x29, 0xE7,        //   Usage Maximum (0xE7)
-  0x15, 0x00,        //   Logical Minimum (0)
-  0x25, 0x01,        //   Logical Maximum (1)
-  0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-  0x95, 0x06,        //   Report Count (6): 6 Key rollover?
-  0x75, 0x08,        //   Report Size (8)
-  0x15, 0x00,        //   Logical Minimum (0)
-  0x25, 0x94,        //   Logical Maximum (0x94) originally 65, 73 supports F13 - F24, 94 for japanese and korean language keys
-  0x05, 0x07,        //   Usage Page (Kbrd/Keypad)
-  0x19, 0x00,        //   Usage Minimum (0x00)
-  0x29, 0x94,        //   Usage Maximum (0x94) originally 65, 73 supports F13 - F24
-  0x81, 0x00,        //   Input (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
+  0x85, 0x01,        //  Report ID (1)
+  // Modifiers (Left Ctrl, Shift, Alt, GUI, Right Ctrl, etc.)
+  0x05, 0x07,       //   Usage Page (Key Codes)
+  0x19, 0xE0,       //   Usage Minimum (224)
+  0x29, 0xE7,       //   Usage Maximum (231)
+  0x15, 0x00,       //   Logical Minimum (0)
+  0x25, 0x01,       //   Logical Maximum (1)
+  0x75, 0x01,       //   Report Size (1)
+  0x95, 0x08,       //   Report Count (8)
+  0x81, 0x02,       //   Input (Data, Variable, Absolute)
+  // Reserved Byte (Padding for Compatibility)
+  0x95, 0x01,       //   Report Count (1)
+  0x75, 0x08,       //   Report Size (8)
+  0x81, 0x01,       //   Input (Constant) 
+  // 6-Key Rollover
+  0x95, 0x06,       //   Report Count (6)
+  0x75, 0x08,       //   Report Size (8)
+  0x15, 0x00,       //   Logical Minimum (0)
+  0x25, 0x94,       //   Logical Maximum (148)
+  0x05, 0x07,       //   Usage Page (Key Codes)
+  0x19, 0x00,       //   Usage Minimum (0)
+  0x29, 0x94,       //   Usage Maximum (148)
+  0x81, 0x00,       //   Input (Data, Array)
+
     // LEDs?
   0x05, 0x08, // USAGE_PAGE (LEDs)
   0x19, 0x01, // USAGE_MINIMUM (Num Lock)
@@ -126,17 +134,6 @@ const uint8_t hid_report_descriptor[] = {
   0x95, 0x03,        //     Report Count (3)
   0x81, 0x06,        //     Input (Data,Var,Rel,No Wrap,Linear,Preferred State,No Null Position)
   0xC0,              //   End Collection
-  0x09, 0x3C,        //   Usage (Motion Wakeup)
-  0x05, 0xFF,        //   Usage Page (Reserved 0xFF)
-  0x09, 0x01,        //   Usage (0x01)
-  0x15, 0x00,        //   Logical Minimum (0)
-  0x25, 0x01,        //   Logical Maximum (1)
-  0x75, 0x01,        //   Report Size (1)
-  0x95, 0x02,        //   Report Count (2)
-  0xB1, 0x22,        //   Feature (Data,Var,Abs,No Wrap,Linear,No Preferred State,No Null Position,Non-volatile)
-  0x75, 0x06,        //   Report Size (6)
-  0x95, 0x01,        //   Report Count (1)
-  0xB1, 0x01,        //   Feature (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
   0xC0,              // End Collection
   // Report ID 4: Custom HID communication pipe
   0x05, 0x01,                    // USAGE_PAGE (counted buffer?)
